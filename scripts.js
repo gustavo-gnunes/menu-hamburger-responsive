4eade45d910ecc2e82cb2740e7c2331ddcfa2c7d
gustavo genaro <28142@martinopolis.sp.gov.br>
2021-03-12 15:14:45 -0300

    :tada: initial commit

--- scripts.js
+++ scripts.js
@@ -2,10 +2,8 @@ const Modal = {
   menuHamburgersOnOff() {
     if(document.querySelector('.menu-container.active')) {
       document.querySelector('.menu-container').classList.remove('active')
-      // console.log('removeu active')
     } else {
       document.querySelector('.menu-container').classList.add('active')
-      // console.log('add active')
     }
   }
 }
